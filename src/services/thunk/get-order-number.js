3967d0cf6b0a877374939eadc57686a1915f4050
stan99 <stan99-cheb@yandex.ru>
2023-06-03 10:42:53 +0300

    refactor/добавление accessToken при отправке заказа на сервер

--- src/services/thunk/get-order-number.js
+++ src/services/thunk/get-order-number.js
@@ -3,12 +3,13 @@ import * as api from "../../utils/api";
 
 export const getOrderNumber = createAsyncThunk(
   'order/fetch',
-  async (idIngredients, { rejectWithValue }) => {
+  async ({ idIngredients, accessToken }, { rejectWithValue }) => {
     try {
       const response = await api.request('/orders', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + accessToken,
         },
         body: JSON.stringify({
           ingredients: idIngredients
